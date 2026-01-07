76088fd2d4625ac668e63278cf8a049ae7459d6e
feed619 <azimovpro@gmail.com>
2026-01-07 16:12:18 +0300

    Добавил FormElement

---
+++ LayoutML/elements/FormElement.py
@@ -0,0 +1,34 @@
+from LayoutML.base import HTMLElement
+
+
+class FormElement(HTMLElement):
+
+    def __init__(self, form_type: str = "text", boolean_attributes=[], **kwargs):
+        """ """
+        super().__init__(boolean_attributes=boolean_attributes, **kwargs)
+
+        self.tag = "input"
+        self.form_type = form_type
+
+    def get_attributes_string(self):
+        """
+        Генерирует строку специфических атрибутов формы.
+
+        Returns
+        -------
+        Строку html атрибутов
+        """
+        attrs = []
+        attrs_str = super().get_attributes_string()
+
+        if self.form_type:
+            attrs.append(f'type="{self.form_type}"')
+        return " ".join(attrs) + " " + attrs_str
+
+    def render(self) -> str:
+        """Рендеринг Label"""
+        attrs = self.get_attributes_string()
+        if attrs:
+            return f"<{self.tag} {attrs}></{self.tag}>"
+        else:
+            return f"<{self.tag}></{self.tag}>"
